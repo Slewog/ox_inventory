377ea678baff6ffb6fae20b010b79713a56321bd
Slewog <tibs-rps4101@hotmail.com>
2023-06-27 23:59:57 +0200

    Update evidence.lua
    Change position inside gabz mrpd

--- data/evidence.lua
+++ data/evidence.lua
@@ -1,14 +1,18 @@
 return {
 	{
-		coords = vec3(458.97, -982.79, 30.68),
+
+		coords = vec3(472.95, -995.25, 25.50),
 		target = { -- qtarget support
 			name = 'mrpd_evidence', -- name of zone must be uniuqe
-			loc = vec3(459.07, -984.07, 30.69),
-			length = 1.4,
-			width = 3.2,
-			heading = 0,
-			minZ = 29.09,
-			maxZ = 31.89
+			icon = 'fa-solid fa-box-archive',
+			loc = vec3(471.90, -995.30, 25.50),
+			length = 0.82,
+			distance = 1.0,
+			width = 4.88,
+			heading = 90,
+			debug = false,
+			minZ = 25.30,
+			maxZ = 27.45
 		}
 	}
 }
