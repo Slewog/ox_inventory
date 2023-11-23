eff54db3db9cf6b7f29f01d095b077e923b40be4
Slewog <tibs-rps4101@hotmail.com>
2023-11-23 19:29:24 +0100

    lspd evidence pos change

--- data/evidence.lua
+++ data/evidence.lua
@@ -1,14 +1,34 @@
 return {
+	-- {
+
+	-- 	coords = vec3(472.95, -995.25, 25.50),
+	-- 	target = { -- qtarget support
+	-- 		name = 'mrpd_evidence_gabz', -- name of zone must be uniuqe
+	-- 		icon = 'fa-solid fa-box-archive',
+	-- 		loc = vec3(471.90, -995.30, 25.50),
+	-- 		length = 0.82,
+	-- 		distance = 1.0,
+	-- 		width = 4.88,
+	-- 		heading = 90,
+	-- 		debug = false,
+	-- 		minZ = 25.30,
+	-- 		maxZ = 27.45
+	-- 	}
+	-- },
 	{
-		coords = vec3(458.97, -982.79, 30.68),
+
+		coords = vec3(564.9288, 6.6044, 69.3572),
 		target = { -- qtarget support
-			name = 'mrpd_evidence', -- name of zone must be uniuqe
-			loc = vec3(459.07, -984.07, 30.69),
-			length = 1.4,
-			width = 3.2,
-			heading = 0,
-			minZ = 29.09,
-			maxZ = 31.89
+			name = 'mrpd_evidence_pug', -- name of zone must be uniuqe
+			icon = 'fa-solid fa-box-archive',
+			loc = vec3(563.05, 4.34, 68.36),
+			length = 7.6,
+			distance = 1.0,
+			width = 1.25,
+			heading = 340,
+			debug = false,
+			minZ = 68.36,
+			maxZ = 70.56
 		}
 	}
 }
